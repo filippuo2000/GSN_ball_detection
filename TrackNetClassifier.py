930b75ad2992c820bbceb3cff9d985950fbaa477
maciejWUT <mciek.sieradzki@gmail.com>
2024-05-15 17:33:20 +0200

    moved common variables in TrackNetClassifier to __init__

--- TrackNetClassifier.py
+++ TrackNetClassifier.py
@@ -9,15 +9,17 @@ class TrackNetClassifier(pl.LightningModule):
         super().__init__()
         self.model = model
         self.lr = 1
+        self.size = (360, 640)
+        self.sigma = 10
 
     def forward(self, x):
         return self.model(x)
 
-    def gaussian_distribution(self, xy, sigma, size):
+    def gaussian_distribution(self, xy):
         device = torch.device('cuda') if xy[0].is_cuda else torch.device('cpu')
 
         batch_size = xy[0].size(0)
-        xx, yy = torch.meshgrid(torch.arange(size[0], device=device), torch.arange(size[1], device=device))
+        xx, yy = torch.meshgrid(torch.arange(self.size[0], device=device), torch.arange(self.size[1], device=device))
         xx = xx.unsqueeze(0).expand(batch_size, -1, -1)  # Expand dimensions for broadcasting
         yy = yy.unsqueeze(0).expand(batch_size, -1, -1)
 
@@ -29,7 +31,7 @@ class TrackNetClassifier(pl.LightningModule):
 
         xy_batch = torch.cat(xy_batch, dim=3)  # Correct concatenation
 
-        distances = ((xx - xy_batch[:, :, :, 0]) ** 2 + (yy - xy_batch[:, :, :, 1]) ** 2) / (2 * sigma ** 2)
+        distances = ((xx - xy_batch[:, :, :, 0]) ** 2 + (yy - xy_batch[:, :, :, 1]) ** 2) / (2 * self.sigma ** 2)
 
         G = torch.exp(-distances) * 255
         G = torch.floor(G)
@@ -44,7 +46,7 @@ class TrackNetClassifier(pl.LightningModule):
         device = torch.device('cuda') if y.is_cuda else torch.device('cpu')
 
         batch_size = y.size(0)
-        output_y = torch.zeros((batch_size, 256, 360, 640), device=device)
+        output_y = torch.zeros((batch_size, 256, self.size[0], self.size[1]), device=device)
         index_tensor = torch.arange(256, device=device).view(1, -1, 1, 1)
         index_tensor = index_tensor.expand(batch_size, -1, -1, -1)
         mask = index_tensor == y.unsqueeze(1)
@@ -55,19 +57,11 @@ class TrackNetClassifier(pl.LightningModule):
     def compute_loss(self, x, y):
         return  F.binary_cross_entropy(x, y)
 
-        #return F.binary_cross_entropy(x, y)
-
 
     def common_step(self, batch, batch_idx):
         x, y = batch
-        #print(y[1].shape)
-        y = self.gaussian_distribution(y, sigma=10, size=(360, 640))
+        y = self.gaussian_distribution(y)
         y = self.y_3d(y)
-        #print(y[1])
-        #print(x.shape)
-        #print(y)
-        #print(type(y))
-        #print(y.shape)
         outputs = self(x)
         loss = self.compute_loss(outputs, y)
         return loss, outputs, y
@@ -101,4 +95,4 @@ class TrackNetClassifier(pl.LightningModule):
 
     def configure_optimizers(self):
         #return torch.optim.Adam(self.parameters(), lr = 1e-1)
-        return torch.optim.Adadelta(self.parameters(), lr=1)
+        return torch.optim.Adadelta(self.parameters(), lr=self.lr)
