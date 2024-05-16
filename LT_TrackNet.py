fc0b378f3b4af96ae03a8f2c58f1233703200d7d
maciejWUT <mciek.sieradzki@gmail.com>
2024-05-16 20:57:05 +0200

    added weights initialization

--- LT_TrackNet.py
+++ LT_TrackNet.py
@@ -5,15 +5,22 @@ from TrackNetCallbacks import get_early_stopping, get_checkpoint_callback, Image
 from pytorch_lightning.loggers import WandbLogger
 import pytorch_lightning as pl
 import wandb
-from pytorch_lightning import Trainer
 import multiprocessing
+import torch.nn as nn
 
+
+def initialize_weights(model):
+    for m in model.modules():
+        if isinstance(m, nn.Conv2d):
+            nn.init.uniform_(m.weight, a=-0.05, b=0.05)
+            nn.init.uniform_(m.bias, a=-0.05, b=0.05)
 
 def main():
     wandb.login(key="d2e53441a811808da2ad7a72b2b8c36c99d4afaf", verify=True)
     wandb_logger = WandbLogger(project='TrackNetModel', job_type='train')
     wandb.init(reinit=True)
     model = TrackNet()
+    initialize_weights(model)
     classifier = TrackNetClassifier(model)
     dm = TrackNetDataModule()
     dm.prepare_data()
