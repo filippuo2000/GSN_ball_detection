b4b2d00f079f94361a1000f9077beea0963db9a9
maciejWUT <mciek.sieradzki@gmail.com>
2024-05-16 21:18:04 +0200

    added normalization to dataset

--- TrackNetDataModule.py
+++ TrackNetDataModule.py
@@ -2,6 +2,7 @@ import os
 import pytorch_lightning as pl
 from torch.utils.data.dataloader import DataLoader
 from InitializeDataset import InitializeDataset
+import torchvision.transforms as transforms
 
 class TrackNetDataModule(pl.LightningDataModule):
     def __init__(self):
@@ -20,9 +21,11 @@ class TrackNetDataModule(pl.LightningDataModule):
         self.dataset.stats()
 
     def setup(self, stage=None):
-        self.train_dataset = self.dataset.train_dataset()
-        self.validate_dataset = self.dataset.validate_dataset()
-        self.test_dataset = self.dataset.test_dataset()
+        normalize = transforms.Normalize(mean=[0.5, 0.5, 0.5],  # RGB mean
+                                         std=[0.5, 0.5, 0.5])
+        self.train_dataset = self.dataset.train_dataset(transform=normalize)
+        self.validate_dataset = self.dataset.validate_dataset(transform=normalize)
+        self.test_dataset = self.dataset.test_dataset(transform=normalize)
 
     def train_dataloader(self):
         return DataLoader(self.train_dataset, batch_size=self.batch_size, num_workers = 4)
