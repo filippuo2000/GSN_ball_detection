bc5b51a61d4bdfa2388ab72c4ea44c0ec9ebcb9b
maciejWUT <mciek.sieradzki@gmail.com>
2024-05-30 21:13:46 +0200

    sorted TrackNetMetrics.py and bug caused by GaussianBlur

--- TrackNetClassifier.py
+++ TrackNetClassifier.py
@@ -14,7 +14,7 @@ class TrackNetClassifier(pl.LightningModule):
         self.lr = 1
         self.size = (360, 640)
         self.sigma = 3.3
-        self.metrics = MyMetrics()
+        self.metrics = MyMetrics().to(device=self.device)
         self.cross_entropy = CrossEntropy(epsilon=1e-10)
     def forward(self, x):
         return self.model(x)
@@ -66,16 +66,13 @@ class TrackNetClassifier(pl.LightningModule):
         locations = torch.zeros((2, feature_map.shape[0])) - 1
         feature_map = torch.transpose(feature_map, 1, 2)
         feature_map = feature_map.detach().numpy()
-        #feature_map *= 255
         feature_map = feature_map.astype(np.uint8)
         #print("feature map shape is: ", feature_map.shape)
         ret, heatmap = cv2.threshold(feature_map[:], 127, 255, cv2.THRESH_BINARY)
-        #print("heatmap shape is: ", heatmap.shape)
-        heatmap = cv2.GaussianBlur(heatmap, (5, 5), 0, 0)
-        #print("heatmap shape is: ", heatmap.shape)
-        # show_img(np.expand_dims(heatmap, axis=0))
+
         for i in range(feature_map.shape[0]):
-            circles = cv2.HoughCircles(heatmap[i], cv2.HOUGH_GRADIENT, dp=1, minDist=1, param1=100, param2=0.9,
+            heatmap_i =  cv2.GaussianBlur(heatmap[i], (5, 5), 0, 0)
+            circles = cv2.HoughCircles(heatmap_i, cv2.HOUGH_GRADIENT, dp=1, minDist=1, param1=100, param2=0.9,
                                        minRadius=1,
                                        maxRadius=15)
             x, y = -10, -10
@@ -106,12 +103,10 @@ class TrackNetClassifier(pl.LightningModule):
         loss, outputs, y = self.common_step(batch, batch_idx)
         #locate xy coordinates basing on resulting feature map
         preds = self.postprocess_output(outputs)
+        self.metrics = self.metrics.to(device=self.device)
         preds = preds.to(device=self.device)
-        metrics = self.metrics(preds, y)
-        acc = metrics['accuracy']
-        precision = metrics['precision']
-        recall = metrics['recall']
-        return loss, acc, precision, recall
+        self.metrics.update(preds, y)
+        return loss
 
     def training_step(self, batch, batch_idx):
         loss, outputs, y = self.common_step(batch, batch_idx)
@@ -120,21 +115,27 @@ class TrackNetClassifier(pl.LightningModule):
 
 
     def validation_step(self, batch, batch_idx):
-        loss, acc, precision, recall = self.common_test_valid_step(batch, batch_idx)
+        loss = self.common_test_valid_step(batch, batch_idx)
         self.log('val_loss', loss, prog_bar=True)
-        self.log('val_acc', acc, prog_bar=True)
-        self.log('val_precision', precision, prog_bar=True)
-        self.log('val_recall', recall, prog_bar=True)
-        return loss
 
+    def on_validation_epoch_end(self) -> None:
+        results = self.metrics.compute()
+        self.log('val_acc', results['accuracy'], prog_bar=True)
+        self.log('val_precision', results['precision'], prog_bar=True)
+        self.log('val_recall', results['recall'], prog_bar=True)
+        self.metrics.reset()
 
     def test_step(self, batch, batch_idx):
-        loss, acc, precision, recall = self.common_test_valid_step(batch, batch_idx)
+        loss = self.common_test_valid_step(batch, batch_idx)
         self.log('test_loss', loss, prog_bar=True)
-        self.log('test_acc', acc, prog_bar=True)
-        self.log('test_precision', precision, prog_bar=True)
-        self.log('test_recall', recall, prog_bar=True)
-        return loss
+
+
+    def on_test_epoch_end(self) -> None:
+        results = self.metrics.compute()
+        self.log('test_acc', results['accuracy'], prog_bar=True)
+        self.log('test_precision', results['precision'], prog_bar=True)
+        self.log('test_recall', results['recall'], prog_bar=True)
+        self.metrics.reset()
 
 
     def configure_optimizers(self):
--- TrackNetMetrics.py
+++ TrackNetMetrics.py
@@ -5,15 +5,17 @@ import torch
 class MyMetrics(Metric):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.add_state("precision", default=torch.tensor(0), dist_reduce_fx="sum")
-        self.add_state("recall", default=torch.tensor(0), dist_reduce_fx="sum")
-        self.add_state("accuracy", default=torch.tensor(0), dist_reduce_fx="sum")
+        self.add_state("true_positives", default=torch.tensor(0), dist_reduce_fx="sum")
+        self.add_state("false_positives", default=torch.tensor(0), dist_reduce_fx="sum")
+        self.add_state("false_negatives", default=torch.tensor(0), dist_reduce_fx="sum")
+        self.add_state("true_negatives", default=torch.tensor(0), dist_reduce_fx="sum")
+        self.add_state("total", default=torch.tensor(0), dist_reduce_fx="sum")
+
 
     # expects tensors of shape [Batch_size, 2] 2 because x and y is needed
-    #
     def update(self, preds, target) -> None:
         # CHECK if this is correct !!!!
-        self.total = target[0].shape[0]  # batch size
+        self.total += target[0].shape[0]  # batch size
 
         target = torch.stack(target)
         if preds.shape != target.shape:
@@ -25,8 +27,8 @@ class MyMetrics(Metric):
 
         # set [-10,-10] for not detected in preds
         # set [-100, -100] for not detected in target
-        self.incorrect = (torch.sqrt((preds-target)**2).sum(dim=0)>5).sum() # helper
-        self.tp = (torch.sqrt((preds-target)**2).sum(dim=0)<=5).sum() # TP
+        incorrect = (torch.sqrt(((preds-target)**2).sum(dim=0))>10).sum() # helper
+        self.true_positives += (torch.sqrt(((preds-target)**2).sum(dim=0))<=10).sum() # TP
 
         preds_ = preds.sum(dim=0)
         target_ = target.sum(dim=0)
@@ -34,17 +36,34 @@ class MyMetrics(Metric):
         mask_fn = (preds_ < 0) & (target_ > 0) # not detected when should have
         mask_fp = (preds_ > 0) & (target_ < 0) # detected when it should not have
         mask_tn = (preds_ < 0) & (target_ < 0) # not detected when should not have
-        self.fn = mask_fn.sum() # FN
-        self.tn = mask_tn.sum() # TN
-        self.fp = self.incorrect - self.tn - self.fn # FP
+        self.false_negatives += mask_fn.sum() # FN
+        self.true_negatives += mask_tn.sum() # TN
+        self.false_positives += incorrect - mask_tn.sum() - mask_fn.sum() # FP
 
 
-    def compute(self) -> torch.Tensor:
+    def compute(self):
+        #print('total:\t', self.total)
+        #print('true_positives:\t', self.true_positives)
+        #print('true_negatives:\t', self.true_negatives)
+        #print('false_positives:\t', self.false_positives)
+        #print('false_negative:\t', self.false_negatives)
+        accuracy = (self.true_positives + self.true_negatives) / self.total
+        precision = self.true_positives / (self.true_positives + self.false_positives + 1e-10)
+        recall = self.true_positives / (self.true_positives + self.false_negatives + 1e-10)
+
         return {
-            "precision": self.tp.float() / (self.tp.float() + self.fp.float()),
-            "recall": self.tp.float() / (self.tp.float() + self.fn.float()),
-            "accuracy": self.tp.float() / self.total
+            "accuracy": accuracy,
+            "precision": precision,
+            "recall": recall
         }
+
+    def reset(self):
+        # Reset the state variables
+        self.true_positives = torch.tensor(0.0)
+        self.false_positives = torch.tensor(0.0)
+        self.false_negatives = torch.tensor(0.0)
+        self.true_negatives = torch.tensor(0.0)
+        self.total = torch.tensor(0.0)
 
 # precision = TP / TP+FP
 # recall = TP / TP+FN
